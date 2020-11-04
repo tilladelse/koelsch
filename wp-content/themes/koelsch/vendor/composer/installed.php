ee764e294b87c15cf31eb87ea7ed360700ca05cb
JP Cozby <jpmacbook@JPs-MacBook-Pro.hsd1.wa.comcast.net>
2020-11-03 17:08:03 -0800

    track vendor files

---
+++ wp-content/themes/koelsch/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ef44853bca5228866c582fef01e6f481e2cf186f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ef44853bca5228866c582fef01e6f481e2cf186f',
+    ),
+    'cmb2/cmb2' => 
+    array (
+      'pretty_version' => 'v2.7.0',
+      'version' => '2.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '438c26dda7ac110d0741448d65614b3ec6379034',
+    ),
+    'jjgrainger/wp-custom-post-type-class' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'f866c3a35ec8041f8ccded863d064778e2080edb',
+    ),
+  ),
+);
