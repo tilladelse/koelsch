4cd2d8c220f3343809904bade30d66708ad64603
tilladelse <jp@tilladelse.com>
2020-12-10 12:35:59 -0800

    ad page container class filter

--- wp-content/themes/koelsch/template-parts/content-page.php
+++ wp-content/themes/koelsch/template-parts/content-page.php
@@ -2,14 +2,16 @@
   $bg = get_post_meta(get_the_id(), 'background_image', true);
   $bg = isset($bg[0]) ? $bg[0] : false;
   $src = $bg && isset($bg['image']) ? $bg['image'] : '';
+  $color = $bg && isset($bg['color']) ? ' '.$bg['color'] : '';
   $hzAlign = $bg && isset($bg['horiz_align']) ? $bg['horiz_align'] : '';
   $vtAlign = $bg && isset($bg['vert_align']) ? $bg['vert_align'] : '';
   $rule = $src ? ' style="background: url('.$src.') no-repeat;' : '';
   $rule .= $src ? ' background-position: '.$hzAlign.' '.$vtAlign.';"' : '';
+  $containerClass = apply_filters('koelsch_container_class', 'container-md');
 ?>
-<div class="page-content"<?php echo $rule;?> id="page_content">
+<div class="page-content<?php echo $color;?>"<?php echo $rule;?> id="page_content">
 <section class="section">
-  <div class="container-md">
+  <div class="<?php echo $containerClass;?>">
     <?php the_content(); ?>
   </div>
 </section>
