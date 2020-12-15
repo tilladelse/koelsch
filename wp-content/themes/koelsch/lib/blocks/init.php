78b094799ce14bda8f300f228f4be7ebca6da724
tilladelse <jp@tilladelse.com>
2020-12-14 16:31:16 -0800

    recategorize blocks

--- wp-content/themes/koelsch/lib/blocks/init.php
+++ wp-content/themes/koelsch/lib/blocks/init.php
@@ -56,33 +56,33 @@ function wp_acf_init_block_types() {
                   		'align'		=> array('wide','full'),
                   	)
         ));
-		
-		
+
+
 		/* new Content Blocks */
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'single_image_and_content',
 			'title'				=> __('Single Image & Content'),
 			'render_template'   => 'blocks/content-blocks/single_image_and_content.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'media-document',
-            'keywords'          => array( 'image', 'content' ),
+      'icon'              => 'media-document',
+      'keywords'          => array( 'image', 'content' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
 				'align' => array('left', 'right'),
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'content_area',
 			'title'				=> __('Content Area'),
 			'render_template'   => 'blocks/content-blocks/content_area.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
-            'icon'              => 'layout',
-            'keywords'          => array( 'content', 'area' ),
+      'icon'              => 'layout',
+      'keywords'          => array( 'content', 'area' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
@@ -90,67 +90,67 @@ function wp_acf_init_block_types() {
 				'align' => array('left', 'center', 'right'),
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'gold_line',
 			'title'				=> __('Gold Line'),
 			'render_template'   => 'blocks/content-blocks/gold_line.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'minus',
-            'keywords'          => array( 'gold', 'line' ),
+      'icon'              => 'minus',
+      'keywords'          => array( 'gold', 'line' ),
 			'supports'			=> array(
 				'align' => array('left', 'center', 'right'),
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'goldtitle',
 			'title'				=> __('Gold Title'),
 			'render_template'   => 'blocks/content-blocks/goldtitle.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'minus',
-            'keywords'          => array( 'gold', 'title' ),
+      'icon'              => 'minus',
+      'keywords'          => array( 'gold', 'title' ),
 			'supports'			=> array(
 				'align' => false,
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'button',
 			'title'				=> __('Button'),
 			'render_template'   => 'blocks/content-blocks/button.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'button',
+      'icon'              => 'button',
 			'align'	          	=> 'wide',
-            'keywords'          => array( 'button' )
+      'keywords'          => array( 'button' )
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'image_with_caption',
 			'title'				=> __('Image With Caption'),
 			'render_template'   => 'blocks/content-blocks/image_with_caption.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
-            'icon'              => 'cover-image',
-            'keywords'          => array( 'image', 'caption' ),
+      'icon'              => 'cover-image',
+      'keywords'          => array( 'image', 'caption' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
 				'align' => array('left', 'right'),
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'map',
 			'title'				=> __('Map'),
 			'render_template'   => 'blocks/content-blocks/map.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
-            'icon'              => 'admin-site-alt3',
-            'keywords'          => array( 'map' ),
+      'icon'              => 'admin-site-alt3',
+      'keywords'          => array( 'map' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => false,
@@ -159,105 +159,105 @@ function wp_acf_init_block_types() {
 			'enqueue_style' => 'https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css',
 			/*'enqueue_script' => 'https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js',*/
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'subsection',
 			'title'				=> __('Subsection'),
 			'render_template'   => 'blocks/content-blocks/subsection.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
 			'align'	          	=> false,
-            'icon'              => 'cover-image',
-            'keywords'          => array( 'image', 'caption' ),
+      'icon'              => 'cover-image',
+      'keywords'          => array( 'image', 'caption' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true
 				)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'pv_slideshow',
 			'title'				=> __('Photo & Video Slideshow'),
 			'render_template'   => 'blocks/content-blocks/slideshow.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'images-alt2',
+      'icon'              => 'images-alt2',
 			'align'	          	=> 'wide',
             'keywords'          => array( 'photo', 'vodeo', 'slideshow' )
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'masonry_images',
 			'title'				=> __('Masonry Images'),
 			'render_template'   => 'blocks/content-blocks/masonry_images.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'edit',
-            'icon'              => 'instagram',
+      'icon'              => 'instagram',
 			'align'	          	=> 'wide',
             'keywords'          => array( 'photo', 'vodeo', 'masonry', 'images' )
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'image_and_content_set',
 			'title'				=> __('Image & Content Set'),
 			'render_template'   => 'blocks/content-blocks/image_and_content_set.php',
-			'category'          => 'formatting',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
-            'icon'              => 'media-document',
-            'keywords'          => array( 'image', 'content' ),
+      'icon'              => 'media-document',
+      'keywords'          => array( 'image', 'content' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
 				'align' => array('left', 'right'),
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'main_image',
 			'title'				=> __('Main Image & Content'),
 			'render_template'   => 'blocks/content-blocks/main_image.php',
 			'category'          => 'formatting',
 			'mode'              => 'preview',
-            'icon'              => 'media-document',
-            'keywords'          => array( 'image', 'content' ),
+      'icon'              => 'media-document',
+      'keywords'          => array( 'image', 'content' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
 				'align' => false,
 			)
 		));
-		
+
 		acf_register_block_type( array(
 			'name'				=> 'secondary_image',
 			'title'				=> __('Secondary Image & Content'),
 			'render_template'   => 'blocks/content-blocks/secondary_image.php',
 			'category'          => 'formatting',
 			'mode'              => 'preview',
-            'icon'              => 'media-document',
-            'keywords'          => array( 'image', 'content' ),
+      'icon'              => 'media-document',
+      'keywords'          => array( 'image', 'content' ),
 			'supports'			=> array(
 				'mode' => true,
 				'jsx' => true,
 				'align' => false,
 			)
 		));
-		
+
 		acf_register_block_type( array(
-			'name'				=> 'conatiner',
+			'name'				=> 'container',
 			'title'				=> __('Container'),
-			'render_template'   => 'blocks/content-blocks/conatiner.php',
-			'category'          => 'formatting',
+			'render_template'   => 'blocks/content-blocks/container.php',
+			'category'          => 'koelsch',
 			'mode'              => 'preview',
-            'icon'              => 'welcome-widgets-menus',
-            'keywords'          => array( 'conatiner' ),
+      'icon'              => 'welcome-widgets-menus',
+      'keywords'          => array( 'container' ),
 			'supports'			=> array(
 				'mode' => false,
 				'jsx' => true,
 				'align' => false,
 			)
 		));
-		
-		
+
+
 
         acf_register_block_type(array(
             'name'              => 'round-image',
