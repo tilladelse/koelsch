254036613e2af96ce88cdb0686dcc73293659265
tilladelse <jp@tilladelse.com>
2020-12-17 17:35:49 -0800

    fix margin on image with caption

--- wp-content/themes/koelsch/lib/blocks/content-blocks/image_with_caption.php
+++ wp-content/themes/koelsch/lib/blocks/content-blocks/image_with_caption.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Image With Caption Block Template.
  *
@@ -7,7 +7,7 @@
  * @param   bool $is_preview True during AJAX preview.
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
- 
+
  // Create class attribute allowing for custom "className" and "align" values.
 $classes = '';
 if( !empty($block['className']) ) {
@@ -19,9 +19,10 @@ if( !empty($block['align']) ) {
 }
 
 if( $size = get_field('size') ) {
-	$classes .= sprintf( ' %s', $size );    
+	$classes .= sprintf( ' %s', $size );
 }
-if($has_margins = get_field('has_margins')) $classes .= ' margins'; 
+
+if($has_margins = get_field('margins')) $classes .= ' margins';
 $container_classes_before = '<div class="container-md">';
 $container_classes_after = '</div>';
 
@@ -42,7 +43,7 @@ $img = get_field('image'); ?>
 		<div class="col text-col">
 			<div class="text-box">
 				<?php if($caption_text) echo '<h2>'.$caption_text.'</h2>'; ?>
-				<?php if($btn_name && $btn_url) echo '<a class="btn-outline" href="'.esc_url($btn_url).'">'.esc_attr($btn_name).'</a>'; ?>				
+				<?php if($btn_name && $btn_url) echo '<a class="btn-outline" href="'.esc_url($btn_url).'">'.esc_attr($btn_name).'</a>'; ?>
 			</div>
 		</div>
 		<?php } ?>
