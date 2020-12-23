b44321e2f51e401a012b20f032c14646fe2b1d00
tilladelse <jp@tilladelse.com>
2020-12-23 14:18:41 -0800

    gform button styles

--- wp-content/themes/koelsch/lib/frontend-functions.php
+++ wp-content/themes/koelsch/lib/frontend-functions.php
@@ -402,6 +402,33 @@ function get_living_type_seal(){
   return false;
 }
 
+/**
+ * Filters the next, previous and submit buttons.
+ * Replaces the forms <input> buttons with <button> while maintaining attributes from original <input>.
+ *
+ * @param string $button Contains the <input> tag to be filtered.
+ * @param object $form Contains all the properties of the current form.
+ *
+ * @return string The filtered button.
+ */
+add_filter( 'gform_next_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_previous_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_submit_button', 'input_to_button', 10, 2 );
+function input_to_button( $button, $form ) {
+    $dom = new DOMDocument();
+    $dom->loadHTML( '<?xml encoding="utf-8" ?>' . $button );
+    $input = $dom->getElementsByTagName( 'input' )->item(0);
+    $new_button = $dom->createElement( 'button' );
+    $new_button->appendChild( $dom->createTextNode( $input->getAttribute( 'value' ) ) );
+    $input->removeAttribute( 'value' );
+    foreach( $input->attributes as $attribute ) {
+        $new_button->setAttribute( $attribute->name, $attribute->value );
+    }
+    $input->parentNode->replaceChild( $new_button, $input );
+
+    return $dom->saveHtml( $new_button );
+}
+
 // function koelsch_get_community_sub_menu($menuID){
 //   $items = wp_get_nav_menu_items($menuID);
 //   _wp_menu_item_classes_by_context( $items );
