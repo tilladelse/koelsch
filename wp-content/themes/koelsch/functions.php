e65c6f6eb7a0deb25b55a4c9be95aa6454f48f6d
tilladelse <jp@tilladelse.com>
2020-12-22 17:04:31 -0800

    add favicon

--- wp-content/themes/koelsch/functions.php
+++ wp-content/themes/koelsch/functions.php
@@ -101,6 +101,28 @@ define('THEME_VERSION', '1.0.0');
 	}
  }
 
+ add_action('wp_head', 'add_koelsch_favicon');
+ function add_koelsch_favicon(){
+   $dir = get_stylesheet_directory_uri();
+   echo '<link rel="apple-touch-icon" sizes="57x57" href="'.$dir . '/assets/images/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="'.$dir . '/assets/images/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="'.$dir . '/assets/images/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="'.$dir . '/assets/images/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="'.$dir . '/assets/images/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="'.$dir . '/assets/images/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="'.$dir . '/assets/images/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="'.$dir . '/assets/images/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="'.$dir . '/assets/images/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="'.$dir . '/assets/images/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="'.$dir . '/assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="'.$dir . '/assets/images/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="'.$dir . '/assets/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="'.$dir . '/assets/images/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="'.$dir . '/assets/images/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">';
+ }
+
 add_theme_support( 'post-thumbnails' );
 add_theme_support('align-wide');
 // Add image sizes.
